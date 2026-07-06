b81de1ee3ff0cde2033456dbb249c6c94daf4cf3
rayen HIDRI <rayen.hidri@gmail.com>
2026-07-06 02:45:26 +0200

    routes interceptées

---
+++ src/app/(backoffice)/intercept/(...)cgv/page.tsx
@@ -0,0 +1,16 @@
+import React from 'react'
+
+const CGVIntercept = () => {
+  return (
+    <div>
+      <h1>CGV intercepted, valide ici les CGV</h1>
+      <p>
+      Utilisateur du backOffice, En utilisant ce service, vous acceptez les précautions d'utilisation.
+        Veuillez les lire attentivement avant d'accéder ou d'utiliser le
+        service.
+      </p>
+    </div>
+  )
+}
+
+export default CGVIntercept
